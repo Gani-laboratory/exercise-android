ff7948f2e05a26121ae4c666c6e0279ca4879b72
ahmadxgani <ahmadxgani01@gmail.com>
2023-10-19 00:27:17 +0700

    add example navigation/routing with intent

--- app/src/main/java/com/reyuki/latihan/MainActivity.kt
+++ app/src/main/java/com/reyuki/latihan/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.reyuki.latihan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,63 +9,19 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var edtWidth: EditText
-    private lateinit var edtHeight: EditText
-    private lateinit var edtLength: EditText
-    private lateinit var btnCalculate: Button
-    private lateinit var tvResult: TextView
-
-    companion object {
-        private const val STATE_RESULT  = "state_result"
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, tvResult.text.toString())
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        edtWidth = findViewById(R.id.edt_width)
-        edtHeight = findViewById(R.id.edt_height)
-        edtLength = findViewById(R.id.edt_length)
-        btnCalculate = findViewById(R.id.btn_calculate)
-        tvResult = findViewById(R.id.tv_result)
 
-        btnCalculate.setOnClickListener(this)
-
-        if (savedInstanceState != null) {
-            val result = savedInstanceState.getString(STATE_RESULT)
-            tvResult.text = result
-        }
+        val btnMoveActivity: Button = findViewById(R.id.btn_mv_activity)
+        btnMoveActivity.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        if (v?.id == R.id.btn_calculate) {
-            val inputLength = edtLength.text.toString().trim()
-            val inputWidth = edtWidth.text.toString().trim()
-            val inputHeight = edtHeight.text.toString().trim()
-
-            var isEmptyFields = false
-            if (inputLength.isEmpty()) {
-                isEmptyFields = true
-                edtLength.error = getString(R.string.errMsg)
-            }
-
-            if (inputHeight.isEmpty()) {
-                isEmptyFields = true
-                edtHeight.error = getString(R.string.errMsg)
-            }
-
-            if (inputWidth.isEmpty()) {
-                isEmptyFields = true
-                edtWidth.error = getString(R.string.errMsg)
-            }
-
-            if (!isEmptyFields) {
-                val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-                tvResult.text = volume.toString()
+        when (v?.id) {
+            R.id.btn_mv_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
             }
         }
     }
