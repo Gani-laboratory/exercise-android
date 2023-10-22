5f44d4bc4228bf90be79b6f10e680161f665cdb6
ahmadxgani <ahmadxgani01@gmail.com>
2023-10-22 18:49:44 +0700

    play with UI interface: style and M3 design theme

--- app/src/main/java/com/reyuki/latihan/MainActivity.kt
+++ app/src/main/java/com/reyuki/latihan/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // setTitle("Try Design System") // you can also set the action bar's title here! instead of from app_name strings.xml
         setContentView(R.layout.activity_main)
     }
 }
