e0f2900bffa2e17341464795cc5310663a50bda9
ahmadxgani <ahmadxgani01@gmail.com>
2023-10-23 14:18:44 +0700

    Revert "wip: https://www.dicoding.com/academies/51/discussions/313743"
    This reverts commit e76f625ccca8a3e0395bc0adb2d6b7391242990d.

--- app/src/main/java/com/reyuki/latihan/ListHeroAdapter.kt
+++ app/src/main/java/com/reyuki/latihan/ListHeroAdapter.kt
@@ -5,19 +5,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class ListHeroAdapter(private val listHero: ArrayList<Hero>, private val onClick: (Hero) -> Unit) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
-    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
         val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
         val tvDesc: TextView = itemView.findViewById(R.id.tv_description)
-
-        fun bind(hero: Hero) {
-            itemView.setOnClickListener {
-                onClick(hero)
-            }
-        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -32,5 +28,15 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>, private val onClick
         holder.imgPhoto.setImageResource(photo)
         holder.tvName.text = name
         holder.tvDesc.text = description
+
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listHero[holder.adapterPosition]) }
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
     }
 }
